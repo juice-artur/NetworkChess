9f7f879368d5d3d701ddad7360260aec58dc3a91
asasin.lol10@gmail.com <JJ-STU-DIO>
2021-10-25 18:15:06 +0300

    Add Chess

--- Server/Server.cs
+++ Server/Server.cs
@@ -8,6 +8,46 @@ namespace Server
 {
     class Server
     {
+        static int NextMoves(int step, Chess chess)
+        {
+            if (step == 0)
+                return 1;
+            int count = 0;
+            foreach (string moves in chess.YieldValidMoves())
+                count += NextMoves(step - 1, chess.Move(moves));
+            return count;
+        }
+        static string MovesToSend(Chess chess)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (string moves in chess.YieldValidMoves())
+            {
+                sb.AppendLine(moves);
+                sb.ToString();
+            }
+        }
+        static string ChessToAscii(Chess chess)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("  +-----------------+");
+            for (int y = 7; y >= 0; y--)
+            {
+                sb.Append(y + 1);
+                sb.Append(" | ");
+                for (int x = 0; x < 8; x++)
+                    sb.Append(chess.GetFigureAt(x, y) + " ");
+                sb.AppendLine("| ");
+            }
+            sb.AppendLine("  +-----------------+");
+            sb.AppendLine("    a b c d e f g h  ");
+            if (chess.IsCheck)
+                sb.AppendLine("IS CHECK");
+            if (chess.IsCheckmate)
+                sb.AppendLine("IS CHECKMATE");
+            if (chess.IsStalemate)
+                sb.AppendLine("IS STALEMATE");
+            return sb.ToString();
+        }
         static byte[] MessageToByte(string message)
         {
             return Encoding.UTF8.GetBytes(message);
@@ -21,7 +61,7 @@ namespace Server
         static void SendMessage(NetworkStream[] players, string message)
         {
             byte[] data = MessageToByte(message);
-            foreach(var player in players)
+            foreach (var player in players)
             {
                 player.Write(data, 0, data.Length);
             }
@@ -35,12 +75,14 @@ namespace Server
                 IPAddress localAddr = IPAddress.Parse(args[0]);
                 Int32 port = Int32.Parse(args[1]);
 
-
                 server = new TcpListener(localAddr, port);
                 server.Start();
 
                 TcpClient[] players = new TcpClient[2];
                 NetworkStream[] playersStream = new NetworkStream[2];
+                Chess chess = new Chess();
+
+
 
                 players[0] = server.AcceptTcpClient();
                 Console.WriteLine("Player1 connect to server");
@@ -51,9 +93,19 @@ namespace Server
                 players[1] = server.AcceptTcpClient();
                 Console.WriteLine("Player 2 connect to server");
                 playersStream[1] = players[1].GetStream();
-
-
                 SendMessage(playersStream, "Game ready");
+
+                while (true)
+                {
+                    //Console.WriteLine(chess.fen);
+                    //Console.WriteLine(ChessToAscii(chess));
+                    string movesToSend = "";
+                    string move = Console.ReadLine();
+                    if (move == "") break;
+                    chess = chess.Move(move);
+
+                }
+
 
             }
             catch (SocketException e)
