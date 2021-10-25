829c10e84cd7758b5078e97d95612cecf0fa9716
asasin.lol10@gmail.com <JJ-STU-DIO>
2021-10-25 17:17:18 +0300

    - First message in console

--- Client/Client.cs
+++ Client/Client.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Net.Sockets;
+using System.Text;
 
 namespace Client
 {
@@ -12,21 +13,23 @@ namespace Client
                 // Server connection
                 TcpClient tcpClient = new TcpClient();
                 // IP             Port
-                tcpClient.Connect("www.microsoft.com", 80);
-
+                tcpClient.Connect("127.0.0.1", 8888);
+                 byte[] data = new byte[256];
+                StringBuilder response = new StringBuilder();
                 NetworkStream netStream = tcpClient.GetStream();
+                int bytes = netStream.Read(data, 0, data.Length);
+                response.Append(Encoding.UTF8.GetString(data, 0, bytes));
 
-                
+                Console.WriteLine(response.ToString());
 
-                
             }
 
-            catch(SocketException socketException)
+            catch (SocketException socketException)
             {
                 Console.WriteLine($"SocketException {socketException}");
             }
 
-            catch(Exception e)
+            catch (Exception e)
             {
                 Console.WriteLine($"Exception {e}");
             }
--- Server/Server.cs
+++ Server/Server.cs
@@ -1,12 +1,31 @@
 ﻿using System;
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 using ChessRules;
 
 namespace Server
 {
     class Server
     {
+        static byte[] MessageToByte(string message)
+        {
+            return Encoding.UTF8.GetBytes(message);
+        }
+        static void SendMessage(NetworkStream player, string message)
+        {
+            byte[] data = MessageToByte(message);
+            player.Write(data, 0, data.Length);
+        }
+
+        static void SendMessage(NetworkStream[] players, string message)
+        {
+            byte[] data = MessageToByte(message);
+            foreach(var player in players)
+            {
+                player.Write(data, 0, data.Length);
+            }
+        }
         static void Main(string[] args)
         {
             TcpListener server = null;
@@ -16,15 +35,25 @@ namespace Server
                 IPAddress localAddr = IPAddress.Parse(args[0]);
                 Int32 port = Int32.Parse(args[1]);
 
-                
+
                 server = new TcpListener(localAddr, port);
                 server.Start();
 
                 TcpClient[] players = new TcpClient[2];
+                NetworkStream[] playersStream = new NetworkStream[2];
 
                 players[0] = server.AcceptTcpClient();
                 Console.WriteLine("Player1 connect to server");
+                playersStream[0] = players[0].GetStream();
 
+                SendMessage(playersStream[0], "Expect a second player");
+
+                players[1] = server.AcceptTcpClient();
+                Console.WriteLine("Player 2 connect to server");
+                playersStream[1] = players[1].GetStream();
+
+
+                SendMessage(playersStream, "Game ready");
 
             }
             catch (SocketException e)
