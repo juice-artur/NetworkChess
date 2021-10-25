43a3cbfbb346773fd3228c343e570e0e98f95620
asasin.lol10@gmail.com <JJ-STU-DIO>
2021-10-25 15:42:55 +0300

    Base settap project

---
+++ Client/Client.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net.Sockets;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                // Server connection
+                TcpClient tcpClient = new TcpClient();
+                // IP             Port
+                tcpClient.Connect("www.microsoft.com", 80);
+
+                NetworkStream netStream = tcpClient.GetStream();
+
+                
+
+                
+            }
+
+            catch(SocketException socketException)
+            {
+                Console.WriteLine($"SocketException {socketException}");
+            }
+
+            catch(Exception e)
+            {
+                Console.WriteLine($"Exception {e}");
+            }
+
+
+        }
+    }
+}
---
+++ Server/Server.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using ChessRules;
+
+namespace Server
+{
+    class Server
+    {
+        static void Main(string[] args)
+        {
+            TcpListener server = null;
+            try
+            {
+                // IPAddres and port for incoming connections
+                IPAddress localAddr = IPAddress.Parse(args[0]);
+                Int32 port = Int32.Parse(args[1]);
+
+                
+                server = new TcpListener(localAddr, port);
+                server.Start();
+
+                TcpClient[] players = new TcpClient[2];
+
+                players[0] = server.AcceptTcpClient();
+                Console.WriteLine("Player1 connect to server");
+
+
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+            }
+            finally
+            {
+                // Stop listening for new clients.
+                server.Stop();
+            }
+
+        }
+    }
+}
